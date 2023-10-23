93a47436dbadd551c65127f5a4fb0582aaa3bc45
nikimanoledaki <niki.manoledaki@grafana.com>
2023-10-23 14:47:29 +0200

    Move utils to pkg/transport; add pkg/errors

---
+++ pkg/errors/errors.go
@@ -0,0 +1,11 @@
+package errors
+
+import "fmt"
+
+type ErrNotFound struct {
+	BodyContents []byte
+}
+
+func (e ErrNotFound) Error() string {
+	return fmt.Sprintf("status: 404, body: %s", e.BodyContents)
+}
---
+++ pkg/transport/transport.go
@@ -0,0 +1,122 @@
+package transport
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/grafana/grafana-openapi-client-go/pkg/errors"
+)
+
+const (
+	DefaultTimeout = time.Second * 5
+)
+
+type RetryableTransport struct {
+	Transport        http.RoundTripper
+	NumRetries       int
+	RetryTimeout     time.Duration
+	RetryStatusCodes []string
+}
+
+func (t *RetryableTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	var (
+		resp         *http.Response
+		err          error
+		bodyContents []byte
+	)
+
+	for n := 0; n <= t.NumRetries; n++ {
+		if req.Body != nil {
+			bodyContents, _ = io.ReadAll(req.Body)
+			req.Body = io.NopCloser(bytes.NewBuffer(bodyContents))
+		}
+
+		if n != 0 {
+			retryTimeout := t.RetryTimeout
+			if retryTimeout == 0 {
+				retryTimeout = backoff(n)
+			}
+			time.Sleep(retryTimeout)
+		}
+
+		resp, err = t.Transport.RoundTrip(req)
+
+		// If err is not nil, retry again
+		// That's either caused by client policy, or failure to speak HTTP (such as network connectivity problem). A
+		// non-2xx status code doesn't cause an error.
+		if err != nil {
+			continue
+		}
+
+		// read the body (even on non-successful HTTP status codes)
+		bodyContents, err = io.ReadAll(resp.Body)
+		resp.Body.Close() //nolint:errcheck
+
+		// if there was an error reading the body, try again
+		if err != nil {
+			continue
+		}
+
+		shouldRetry, err := matchRetryCode(resp.StatusCode, t.RetryStatusCodes)
+		if err != nil {
+			return resp, err
+		}
+
+		if !shouldRetry {
+			break
+		}
+	}
+	if err != nil {
+		return resp, err
+	}
+
+	switch {
+	case resp.StatusCode == http.StatusNotFound:
+		return resp, errors.ErrNotFound{
+			BodyContents: bodyContents,
+		}
+	case resp.StatusCode >= 400:
+		return resp, fmt.Errorf("status: %d, body: %v", resp.StatusCode, string(bodyContents))
+	}
+
+	return resp, err
+}
+
+func backoff(retries int) time.Duration {
+	return time.Duration(math.Pow(2, float64(retries))) * time.Second
+}
+
+// matchRetryCode checks if the status code matches any of the configured retry status codes.
+func matchRetryCode(gottenCode int, retryCodes []string) (bool, error) {
+	if len(retryCodes) == 0 {
+		retryCodes = []string{"429", "5xx"}
+	}
+
+	gottenCodeStr := strconv.Itoa(gottenCode)
+	for _, retryCode := range retryCodes {
+		if len(retryCode) != 3 {
+			return false, fmt.Errorf("invalid retry status code: %s", retryCode)
+		}
+		matched := true
+		for i := range retryCode {
+			c := retryCode[i]
+			if c == 'x' {
+				continue
+			}
+			if gottenCodeStr[i] != c {
+				matched = false
+				break
+			}
+		}
+		if matched {
+			return true, nil
+		}
+	}
+
+	return false, nil
+}
