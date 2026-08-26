1c43014c9904f6434f8e92064e3a0c28c2d4002e
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2026-08-26 13:55:23 -0400

    Regenerate client for Grafana c07d5009ee6b23c477e4a0cc0ccbd208d677df79 (#167)
    * bingo 0.10
    
    * update to schema of v13.2.1
    
    * update to latest main.

--- scripts/pull-schema.sh
+++ scripts/pull-schema.sh
@@ -3,8 +3,8 @@
 # https://stackoverflow.com/a/246128
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# Release commit for v13.0.0
-GRAFANA_REPO_SHA_COMMIT="e9de482b5110e6e7310a18274c53a0d7d0eee6af"
+# Commit for main (13.3)
+GRAFANA_REPO_SHA_COMMIT="c07d5009ee6b23c477e4a0cc0ccbd208d677df79"
 
 # Pull the schema (stable commit)
 SCHEMA="$(curl -s -L https://raw.githubusercontent.com/grafana/grafana/${GRAFANA_REPO_SHA_COMMIT}/public/api-merged.json)"
